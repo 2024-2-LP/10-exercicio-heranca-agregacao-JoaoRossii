724648e31abb3573688f774382520943a3669b02
JoaoRossii <joao.rossi@sptech.school>
2024-10-31 14:09:18 -0300

    Exercício 100% completo

--- src/main/java/school/sptech/Consultoria.java
+++ src/main/java/school/sptech/Consultoria.java
@@ -56,7 +56,79 @@ public class Consultoria {
     }
 
     public List<Desenvolvedor> buscarPorSalarioMaiorIgualQue(Double salario) {
-        
+        List<Desenvolvedor> salarios = new ArrayList<>();
+        for (Desenvolvedor desenvolvedor : desenvolvedores) {
+            if (desenvolvedor.calcularSalario() >= salario) {
+                salarios.add(desenvolvedor);
+            }
+        }
+        return salarios;
+    }
+
+    public Desenvolvedor buscarMenorSalario() {
+        if (desenvolvedores.isEmpty()) {
+            return null;
+        }
+        Desenvolvedor salarioMenor = this.desenvolvedores.get(0);
+
+        for (Desenvolvedor dev : desenvolvedores) {
+            if (dev.calcularSalario() < salarioMenor.calcularSalario()) {
+                salarioMenor = dev;
+            }
+        }
+        return salarioMenor;
+    }
+
+    public List<Desenvolvedor> buscarPorTecnologia(String tecnologia) {
+        List<Desenvolvedor> devTec = new ArrayList<>();
+        for (Desenvolvedor dev : desenvolvedores) {
+            if (dev instanceof DesenvolvedorWeb) {
+                if(tecnologia.equals(((DesenvolvedorWeb) dev).getBackend())) {
+                    devTec.add(dev);
+                }
+                if(tecnologia.equals(((DesenvolvedorWeb) dev).getFrontend())) {
+                    devTec.add(dev);
+                }
+                if(tecnologia.equals(((DesenvolvedorWeb) dev).getSgbd())) {
+                    devTec.add(dev);
+                }
+            }
+            if (dev instanceof DesenvolvedorMobile) {
+                if(tecnologia.equals(((DesenvolvedorMobile) dev).getLinguagem())) {
+                    devTec.add(dev);
+                }
+                if(tecnologia.equals(((DesenvolvedorMobile) dev).getPlataforma())) {
+                    devTec.add(dev);
+                }
+            }
+        }
+        return devTec;
+    }
+
+    public Double getTotalSalariosPorTecnologia(String tecnologia) {
+        Double salarios = 0.0;
+        for (Desenvolvedor dev : desenvolvedores) {
+            if (dev instanceof DesenvolvedorWeb) {
+                if(tecnologia.equals(((DesenvolvedorWeb) dev).getBackend())) {
+                    salarios += dev.calcularSalario();
+                }
+                if(tecnologia.equals(((DesenvolvedorWeb) dev).getFrontend())) {
+                    salarios += dev.calcularSalario();
+                }
+                if(tecnologia.equals(((DesenvolvedorWeb) dev).getSgbd())) {
+                    salarios += dev.calcularSalario();
+                }
+            }
+            if (dev instanceof DesenvolvedorMobile) {
+                if(tecnologia.equals(((DesenvolvedorMobile) dev).getLinguagem())) {
+                    salarios += dev.calcularSalario();
+                }
+                if(tecnologia.equals(((DesenvolvedorMobile) dev).getPlataforma())) {
+                    salarios += dev.calcularSalario();
+                }
+            }
+        }
+        return salarios;
     }
 
     public String getNome() {
