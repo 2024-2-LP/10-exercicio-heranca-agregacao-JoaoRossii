130e157d742883e964437ebbbc816bf157b74d08
JoaoRossii <joao.rossi@sptech.school>
2024-10-31 13:25:30 -0300

    Atividade enviada

--- src/main/java/school/sptech/Consultoria.java
+++ src/main/java/school/sptech/Consultoria.java
@@ -1,4 +1,85 @@
 package school.sptech;
 
+import school.sptech.especialistas.DesenvolvedorMobile;
+import school.sptech.especialistas.DesenvolvedorWeb;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Consultoria {
+    private String nome;
+    private Integer vagas;
+    private List<Desenvolvedor> desenvolvedores;
+
+    public Consultoria(String nome, Integer vagas, List<Desenvolvedor> desenvolvedors) {
+        this.nome = nome;
+        this.vagas = vagas;
+        this.desenvolvedores = new ArrayList<>();
+    }
+
+    public Consultoria() {
+        this.desenvolvedores = new ArrayList<>();
+    }
+
+    public Boolean contratar (Desenvolvedor desenvolvedor) {
+        if (desenvolvedores.size() < vagas) {
+            this.desenvolvedores.add(desenvolvedor);
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean contratarFullstack(DesenvolvedorWeb desenvolvedor) {
+        if (desenvolvedor.isFullstack()) {
+            this.desenvolvedores.add(desenvolvedor);
+            return true;
+        }
+        return false;
+    }
+
+    public Double getTotalSalarios() {
+        Double Salarios = 0.0;
+        for (Desenvolvedor dev : desenvolvedores) {
+            Salarios += dev.calcularSalario();
+        }
+        return Salarios;
+    }
+
+    public Integer qtdDesenvolvedoresMobile() {
+        Integer qtdDevs = 0;
+        for (Desenvolvedor dev : desenvolvedores) {
+            if (dev instanceof DesenvolvedorMobile) {
+                qtdDevs ++;
+            }
+        }
+        return qtdDevs;
+    }
+
+    public List<Desenvolvedor> buscarPorSalarioMaiorIgualQue(Double salario) {
+        
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getVagas() {
+        return vagas;
+    }
+
+    public void setVagas(Integer vagas) {
+        this.vagas = vagas;
+    }
+
+    public List<Desenvolvedor> getDesenvolvedores() {
+        return desenvolvedores;
+    }
+
+    public void setDesenvolvedores(List<Desenvolvedor> desenvolvedores) {
+        this.desenvolvedores = desenvolvedores;
+    }
 }
--- src/main/java/school/sptech/especialistas/DesenvolvedorWeb.java
+++ src/main/java/school/sptech/especialistas/DesenvolvedorWeb.java
@@ -1,4 +1,64 @@
 package school.sptech.especialistas;
 
-public class DesenvolvedorWeb {
+import school.sptech.Desenvolvedor;
+
+public class DesenvolvedorWeb extends Desenvolvedor {
+    private String backend;
+    private String frontend;
+    private String sgbd;
+    private Integer horasMentoria;
+
+    public DesenvolvedorWeb(String backend, String frontend, String sgbd, Integer horasMentoria) {
+        this.backend = backend;
+        this.frontend = frontend;
+        this.sgbd = sgbd;
+        this.horasMentoria = horasMentoria;
+    }
+
+    public DesenvolvedorWeb() {
+    }
+
+    @Override
+    public Double calcularSalario() {
+        return (qtdHoras * valorHora) + (this.horasMentoria * 300);
+    }
+
+    public Boolean isFullstack() {
+        if (this.backend == null || this.sgbd == null || this.frontend == null) {
+            return false;
+        }
+        return true;
+    }
+
+    public String getBackend() {
+        return backend;
+    }
+
+    public void setBackend(String backend) {
+        this.backend = backend;
+    }
+
+    public String getFrontend() {
+        return frontend;
+    }
+
+    public void setFrontend(String frontend) {
+        this.frontend = frontend;
+    }
+
+    public String getSgbd() {
+        return sgbd;
+    }
+
+    public void setSgbd(String sgbd) {
+        this.sgbd = sgbd;
+    }
+
+    public Integer getHorasMentoria() {
+        return horasMentoria;
+    }
+
+    public void setHorasMentoria(Integer horasMentoria) {
+        this.horasMentoria = horasMentoria;
+    }
 }
